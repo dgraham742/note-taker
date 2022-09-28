efc40664accde18dfd4d984b32619d7fb8e89083
dgraham742 <106363652+dgraham742@users.noreply.github.com>
2022-09-28 18:42:38 -0500

    add heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const path = require('path')
 const express=require('express')
-const { notes } = require('./Develop/db/db.json')
+const { notes } = require('./db/db.json')
 const { Router } = require('express')
 
 
